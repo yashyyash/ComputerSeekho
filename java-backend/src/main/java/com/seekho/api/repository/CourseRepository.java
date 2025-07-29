864cf2978af02ae39898d65d11f03a810a7b311b
Yash <yashnil532@gmail.com>
2025-07-29 23:58:33 +0530

    lol

--- java-backend/src/main/java/com/seekho/api/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.seekho.api.repository;
-
-import com.seekho.api.entity.Courses;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Courses, Integer> {
-}
